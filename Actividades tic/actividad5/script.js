d6215fd07711151ebac423df439099de91da434c
oliverfrdzz <159531418+oliverfrdzz@users.noreply.github.com>
2024-02-18 13:46:52 +0100

    Add files via upload

---
+++ Actividades tic/actividad5/script.js
@@ -0,0 +1,6 @@
+function calcular(){
+    let num1 =  parseInt(prompt("introduzca el primer valor"));
+    let num2 =  parseInt(prompt("introduzca el segundo valor"));
+    alert("Primer valor: "+num1+"\nSegundo valor: "+num2+"\nSuma: "+(num1+num2)+"\nResta: "+(num1-num2)+"\nProducto: "+(num1*num2)+"\nDivisión: "+(num1/num2)+"\nResto: "+(num1%num2));
+}
+
