8e3771175cd25328efbbf2fb43515b8c42c99837
oliverfrdzz <159531418+oliverfrdzz@users.noreply.github.com>
2024-02-18 16:20:05 +0100

    Update script.js

--- Actividades tic/actividad6/script.js
+++ Actividades tic/actividad6/script.js
@@ -2,9 +2,9 @@ function comprobar(){
     let num = parseInt(document.getElementById("num").value, 0);
 
     if(num%2==0){
-        document.getElementById("resultado").innerHTML = "Es par"
+        document.getElementById("resultado").innerHTML = "Es par";
     }
     else{
-        document.getElementById("resultado").innerHTML = "Es impar"
+        document.getElementById("resultado").innerHTML = "Es impar";
     }
 }
